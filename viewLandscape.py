1325db84b17d1643505982fb86536f37610c623c
Mikolaj Figurski <meek.f80@gmail.com>
2019-03-04 11:00:43 -0500

    Isolated readCSV function in viewLandscape

--- viewLandscape.py
+++ viewLandscape.py
@@ -1,16 +1,20 @@
 from mayavi import mlab
 
-
+# Render map
 def viewMap(map, factor=100):
     map = [[j*factor for j in i] for i in map]
     mlab.surf(map)
     mlab.show()
 
-
-if __name__ == '__main__':
-    surf = []
-    file = open('map.csv')
+# Function that reads a float csv
+def readCSV(filename):
+    res = []
+    file = open(filename)
     for line in file.readlines():
         strings = line.strip().split(',')
-        surf.append([float(i) for i in strings])
+        res.append([float(i) for i in strings])
+    return res
+
+if __name__ == '__main__':
+    surf = readCSV('map.csv')
     viewMap(surf, 100)
