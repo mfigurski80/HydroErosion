5c8594a999eeef996f462133107781c0aef23193
Mikolaj Figurski <meek.f80@gmail.com>
2019-03-04 10:54:57 -0500

    converted viewLandscape to importable function, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # HydroErosion
 Python model of rain eroding a landscape.
+
+## File Descriptions
+### generateLandscape.py
+Creates a 2x2 matrix describing height using perlin noise
+
+### viewLandscape.py
+Graphs given 2x2 matrix in 3D
+
+### erodeLandscape.py
+TODO: where all the fun happens
--- viewLandscape.py
+++ viewLandscape.py
@@ -1,13 +1,16 @@
 from mayavi import mlab
 
 
-surf = []
-file = open('map.csv')
-for line in file.readlines():
-    strings = line.strip().split(',')
-    surf.append([float(i)*100 for i in strings])
+def viewMap(map, factor=100):
+    map = [[j*factor for j in i] for i in map]
+    mlab.surf(map)
+    mlab.show()
 
-# print(surf)
 
-mlab.surf(surf)
-mlab.show()
+if __name__ == '__main__':
+    surf = []
+    file = open('map.csv')
+    for line in file.readlines():
+        strings = line.strip().split(',')
+        surf.append([float(i) for i in strings])
+    viewMap(surf, 100)
